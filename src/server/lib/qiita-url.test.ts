6333107cf5ecbb747be75a6efd76362f69e1365d
nagaya <nagaya@a-tm.co.jp>
2026-02-19 14:07:14 +0900

    Configure jest and test infrastructure
    Fix qiita-url.test.ts to mock QIITA_DOMAIN

--- src/server/lib/qiita-url.test.ts
+++ src/server/lib/qiita-url.test.ts
@@ -3,6 +3,16 @@ import { itemUrl } from "./qiita-url";
 describe("itemUrl", () => {
   const id = "c686397e4a0f4f11683d";
   const userId = "Qiita";
+  let originalQiitaDomain: string | undefined;
+
+  beforeEach(() => {
+    originalQiitaDomain = process.env.QIITA_DOMAIN;
+    process.env.QIITA_DOMAIN = "qiita.com";
+  });
+
+  afterEach(() => {
+    process.env.QIITA_DOMAIN = originalQiitaDomain;
+  });
 
   it("returns item url", () => {
     const url = itemUrl({ id, userId });
